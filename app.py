02705cd717f664ffcdac31e9ef7ba47883684ad0
Phan Sovanna <45761736+PhanSovanna@users.noreply.github.com>
2019-06-11 13:40:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 def answer(message):
     bot = ChatBot('Bot')
     trainer = ChatterBotCorpusTrainer(bot)
-    trainer.train('C:\\Users\\Vanna\\Downloads\\Compressed\\jowithme\\greetings.yml')
+    trainer.train('greetings.yml')
     reply = bot.get_response(message)
     return reply
 
